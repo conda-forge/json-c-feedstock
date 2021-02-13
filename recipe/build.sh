9794d97ba691d68258178c03539e7994b7368a97
Chris Burr <christopher.burr@cern.ch>
2021-02-13 09:50:16 +0100

    Use cmake for building on all platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-bash ./autogen.sh
-
 # https://github.com/json-c/json-c/issues/406
 export CPPFLAGS="${CPPFLAGS/-DNDEBUG/}"
 
-./configure --prefix=$PREFIX --host=$HOST --build=$BUILD
+mkdir build
+cd build
+cmake $CMAKE_ARGS ..
 
 make ${VERBOSE_AT}
-make check ${VERBOSE_AT}
+if [ "${build_platform}" = "${target_platform}" ]; then
+    make test ${VERBOSE_AT}
+fi
 make install
 
 # We can remove this when we start using the new conda-build.
